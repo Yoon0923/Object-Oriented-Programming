0a2a358601e0bfe3332f5d8ea0414e09abb14b80
Yoon <yoonsucho.cpe@gmail.com>
2026-08-24 22:48:08 -0700

    Add Lab1 Hello program

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,14 @@
+/***
+Name: Yoonsu Cho
+Course: ECE 2310 Lab: Lab 1
+Description: A simple C++ Hello World program.
+***/
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}
